147097a21c1318919aaf9b68fa22efd849f80f04
Éster Augusto <61546216+gutoFaria@users.noreply.github.com>
2025-11-13 15:52:11 -0300

    configurations enemies and spawn

--- 3d_rpgLearnGame/Assets/Scripts/EnemyScripts/Enemy.cs
+++ 3d_rpgLearnGame/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -5,34 +5,100 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State { Idle, Chasing, Attacking };
+    private State currentState;
+
+
     private NavMeshAgent pathfinder;
     private Transform target;
+    private Material skinMaterial;
+
+    private Color originalColor;
+
+    private float attackDistanceThreshold = .5f;
+    private float timeBetweenAttack = 1;
+
+    private float nextAttackTime;
+    private float myCollisionRadius;
+    private float targetCollisionRadius;
 
     protected override void Start()
     {
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponentInChildren<Renderer>().material;
+        originalColor = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+
+        //targetCollisionRadius =  myCollisionRadius;
 
         StartCoroutine(UpdatePath());
     }
 
     void Update()
     {
-       // pathfinder.SetDestination(target.position);
+        if (Time.time > nextAttackTime)
+        {
+            float sqrDistToTarget = (target.position - transform.position).sqrMagnitude;
+
+            if (sqrDistToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+            {
+                nextAttackTime = Time.time + timeBetweenAttack;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 dirToTarget = (target.position - transform.position).normalized;
+        Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius );
+        //Vector3 attackPosition = target.position;
+
+        float attackSpeed = 4f;
+        float percent = 0;
+
+
+        skinMaterial.color = Color.gray;
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        skinMaterial.color = originalColor;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath()
     {
         float refreshRate = .25f;
-        while(target != null)
+        while (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 0f, target.position.z);
-            if(!dead)
+            if (currentState == State.Chasing)
             {
-                pathfinder.SetDestination(target.position);
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold/2);
+                //Vector3 targetPosition = new Vector3(target.position.x, 0f, target.position.z);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+                }
             }
-            
             yield return new WaitForSeconds(refreshRate);
         }
     }
---
+++ 3d_rpgLearnGame/Assets/Scripts/SpawnScripts/Spawner.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public Enemy enemy;
+
+    private Wave currentWave;
+    private int currentWaveNumber;
+
+    private int enemiesRemainingToSpawn;
+    private int enemiesRemainingAlive;
+    private float nextSpawnTime;
+
+    void Start()
+    {
+        NextWave();
+    }
+
+    void Update()
+    {
+        if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime)
+        {
+            enemiesRemainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.timeBetweenSpawn;
+
+            Enemy spawnEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity);
+            spawnEnemy.OnDeath += OnEnemyDeath;
+        }
+    }
+
+    private void OnEnemyDeath()
+    {
+        enemiesRemainingAlive--;
+
+        if (enemiesRemainingAlive == 0)
+        {
+            NextWave();
+        }
+    }
+
+    private void NextWave()
+    {
+        currentWaveNumber++;
+        print("wave: " + currentWaveNumber);
+        if (currentWaveNumber - 1 < waves.Length)
+        {
+            currentWave = waves[currentWaveNumber - 1];
+
+            enemiesRemainingToSpawn = currentWave.enemyCount;
+            enemiesRemainingAlive = enemiesRemainingToSpawn;
+        }
+    }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public int enemyCount;
+        public float timeBetweenSpawn;
+    }
+}
