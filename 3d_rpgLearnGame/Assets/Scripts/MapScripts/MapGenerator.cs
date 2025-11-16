8ef571c87d9d7a082f2438559750dfba8c61c5ea
Éster Augusto <61546216+gutoFaria@users.noreply.github.com>
2025-11-16 09:21:36 -0300

    generate obstacle

--- 3d_rpgLearnGame/Assets/Scripts/MapScripts/MapGenerator.cs
+++ 3d_rpgLearnGame/Assets/Scripts/MapScripts/MapGenerator.cs
@@ -1,12 +1,20 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
 
-    [Range(0,1)]
+    [Range(0, 1)]
     public float outlinePercent;
+
+    private List<Coord> allTileCoords;
+    private Queue<Coord> shuffledTileCoords;
+
+    public int seed = 10;
 
     void Start()
     {
@@ -15,10 +23,21 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
-        string holderName = "Generated Map";
-        if(transform.Find(holderName))
+        allTileCoords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
         {
-            DestroyImmediate(transform.Find(holderName).gameObject);    
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCoords.Add(new Coord(x, y));
+            }
+        }
+
+        shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
+
+        string holderName = "Generated Map";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
         }
 
         Transform mapHolder = new GameObject(holderName).transform;
@@ -28,11 +47,45 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, 0f, -mapSize.y/2 + 0.5f + y);
+                Vector3 tilePosition = CoordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90));
-                newTile.localScale = Vector3.one * (1-outlinePercent);
+                newTile.localScale = Vector3.one * (1 - outlinePercent);
                 newTile.parent = mapHolder;
             }
+        }
+
+        int obstacleCount = 10;
+
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * .5f, Quaternion.identity);
+            newObstacle.parent = mapHolder;
+        }
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        return new Vector3(-mapSize.x / 2 + 0.5f + x, 0f, -mapSize.y / 2 + 0.5f + y);
+    }
+
+    public Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
         }
     }
 }
